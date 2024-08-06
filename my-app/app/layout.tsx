cf2f769b9c21c34fe1134b063c94961b5ff05df7
Priyanshu Das <priyanshudas01@gmail.com>
2024-08-06 10:41:38 +0530

    Clerk working setup

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// import ConvexClientProvider from "@/providers/ConvexClientProvider";
+
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { Button } from "@/components/ui/button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+  
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          
+          <header>
+            <SignedOut>
+             <Button><SignInButton /></Button>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <main>
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+     
   );
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,6 +3,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <Button>Login</Button>
+    <Button>hi</Button>
   );
 }
